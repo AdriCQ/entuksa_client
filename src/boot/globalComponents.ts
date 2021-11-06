fbb50b456d43f6ab2499e441507b217e74641154
AdriCQ <adrian.capote95@zoho.com>
2021-11-06 13:14:31 -0400

    Title Widget component

--- src/boot/globalComponents.ts
+++ src/boot/globalComponents.ts
@@ -1,9 +1,12 @@
 import { boot } from 'quasar/wrappers';
 import ShopOfferWidget from 'components/widgets/shop/Offer.vue';
 import ShopStoreWidget from 'components/widgets/shop/StoreWidget.vue';
+import TitleWidget from 'src/components/widgets/Title.vue';
 
 export default boot(({ app }) =>
 {
   app.component('offer-widget', ShopOfferWidget);
   app.component('store-widget', ShopStoreWidget);
+  // Title Widget
+  app.component('title-widget', TitleWidget);
 })
---
+++ src/components/widgets/Title.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-card class="no-box-shadow">
+    <q-card-section class="text-center text-grey-9 q-py-xs">
+      <div class="text-h6">{{ data }}</div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang='ts'>
+import { defineComponent, PropType } from 'vue';
+/**
+ * TITLE_WIDGET
+ */
+export default defineComponent({
+  name: 'TitleWidget',
+  props: {
+    data: { type: String as PropType<string>, required: true },
+  },
+  setup (props) { return { ...props } }
+});
+</script>
